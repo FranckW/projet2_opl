bb49c26b0c222c0d42da3baa16cba85a8fc9c66e
FranckW <franck.warlouzet@hotmail.fr>
2016-10-24 13:55:21 +0200

    fixed some bugs, improve speed

--- src/project_2_OPL/BucketAnalyzer.java
+++ src/project_2_OPL/BucketAnalyzer.java
@@ -24,45 +24,55 @@ public class BucketAnalyzer {
 	public static List<File> trainingFiles = new ArrayList<File>();
 	public static List<File> testingFiles = new ArrayList<File>();
 
+	public static Map<File, String> trainingFilesContent = new HashMap<File, String>();
+	public static Map<File, String> testingFilesContent = new HashMap<File, String>();
+
 	public static void main(String[] args) {
 		final File folderTesting = new File("nautilus-testing");
 		final File folderTraining = new File("nautilus-training");
 		getAllFiles(folderTraining, trainingFiles);
 		getAllFiles(folderTesting, testingFiles);
 
-		Map<File, CrashReport> stackTraceTraining = new HashMap<File, CrashReport>();
-		Map<File, CrashReport> stackTraceTesting = new HashMap<File, CrashReport>();
+		Map<String, CrashReport> stackTraceTraining = new HashMap<String, CrashReport>();
+		Map<String, CrashReport> stackTraceTesting = new HashMap<String, CrashReport>();
 		Map<String, String> result = new HashMap<String, String>();
 
-		fillKeywordsMap(stackTraceTesting, testingFiles);
-		fillKeywordsMap(stackTraceTraining, trainingFiles);
+		loadContentOfFiles(testingFiles, testingFilesContent);
+		loadContentOfFiles(trainingFiles, trainingFilesContent);
+
+		fillKeywordsMap(stackTraceTesting, testingFilesContent);
+		fillKeywordsMap(stackTraceTraining, trainingFilesContent);
 
 		int counter = 0;
-		// for (File trainingFile : stackTraceTraining.keySet())
-		// System.out.println(stackTraceTraining.get(trainingFile).toString());
+		// for(File testingFile : testingFilesContent.keySet())
+		// for(File trainingFile : trainingFilesContent.keySet()) {
+		// Double maxValue = 0.0;
 		//
-		// for (File testingFile : stackTraceTesting.keySet()) {
-		// System.out.println(stackTraceTesting.get(testingFile).toString());
+		// Double distance =
+		// StringSimilarity.similarity(testingFilesContent.get(testingFile),
+		// trainingFilesContent.get(trainingFile));
+		// if distance
 		// }
-
-		for (File testingFile : stackTraceTesting.keySet()) {
+		for (File testingFile : testingFilesContent.keySet()) {
 			System.out.println("file number : " + counter++ + " titled : " + testingFile.getName());
 			Map<String, Double> mapMatchValueBucketName = new HashMap<String, Double>();
-			for (File trainingFile : stackTraceTraining.keySet()) {
+			for (File trainingFile : trainingFilesContent.keySet()) {
 				ArrayList<Double> lineMatchValues = new ArrayList<Double>();
-				for (CrashLine crashLineTesting : stackTraceTesting.get(testingFile).getCrashLines())
-					for (CrashLine crashLineTraining : stackTraceTraining.get(trainingFile).getCrashLines())
-						lineMatchValues.add(CrashLineComparator.compareLines(crashLineTesting, crashLineTraining));
+				for (CrashLine crashLineTesting : stackTraceTesting.get(testingFilesContent.get(testingFile))
+						.getCrashLines())
+					if (stackTraceTraining.get(trainingFilesContent.get(trainingFile)) != null)
+						for (CrashLine crashLineTraining : stackTraceTraining
+								.get(trainingFilesContent.get(trainingFile)).getCrashLines())
+							lineMatchValues.add(CrashLineComparator.compareLines(crashLineTesting, crashLineTraining));
 				double sum = 0;
 				for (Double distance : lineMatchValues)
 					sum += distance;
 				mapMatchValueBucketName.put(trainingFile.getParent(), sum);
 			}
 			String bucket = null;
-			Double maxMatchValue = 0.0;
+			Double maxMatchValue = 1000000000.0;
 			for (String bucketName : mapMatchValueBucketName.keySet()) {
-				if (mapMatchValueBucketName.get(bucketName) > maxMatchValue) { //
-					System.out.println(mapMatchValueBucketName.get(bucketName));
+				if (mapMatchValueBucketName.get(bucketName) < maxMatchValue) {
 					maxMatchValue = mapMatchValueBucketName.get(bucketName);
 					bucket = bucketName.substring(bucketName.length() - 10);
 				}
@@ -85,36 +95,21 @@ public class BucketAnalyzer {
 		System.out.println("" + counter);
 	}
 
-	private static void fillKeywordsMap(Map<File, CrashReport> stackTraceMap, List<File> files) {
+	private static void loadContentOfFiles(List<File> files, Map<File, String> filesContent) {
 		FileReader fileReader = null;
 		for (File file : files) {
-			CrashReport crashReport = new CrashReport();
-			if (!stackTraceMap.containsKey(file))
-				stackTraceMap.put(file, crashReport);
-			else
-				return;
 			try {
 				fileReader = new FileReader(file);
 			} catch (FileNotFoundException e) {
 				e.printStackTrace();
 			}
-
 			BufferedReader br = new BufferedReader(fileReader);
 			try {
 				String line = null;
-				CrashLine crashLine = new CrashLine();
-
-				while ((line = br.readLine()) != null) {
-					if (line.startsWith("#")) {
-						stackTraceMap.get(file).addCrashLines(crashLine);
-						crashLine = new CrashLine();
-						Integer index = line.indexOf(" ");
-						Integer crashLineNumber = Integer.parseInt(line.substring(1, index));
-						crashLine.setLineNumber(crashLineNumber);
-					}
-					getAllKeywordsFromLine(line, crashLine);
-				}
-				stackTraceMap.get(file).getCrashLines().add(crashLine);
+				StringBuffer sb = new StringBuffer("");
+				while ((line = br.readLine()) != null)
+					sb.append(line + "\n");
+				filesContent.put(file, sb.toString());
 			} catch (IOException e) {
 				e.printStackTrace();
 			} finally {
@@ -125,6 +120,35 @@ public class BucketAnalyzer {
 				}
 			}
 		}
+	}
+
+	private static void fillKeywordsMap(Map<String, CrashReport> stackTraceMap, Map<File, String> filesContent) {
+		String fileContent = null;
+		for (File file : filesContent.keySet()) {
+			fileContent = filesContent.get(file);
+			CrashReport crashReport = new CrashReport();
+			if (!stackTraceMap.containsKey(fileContent))
+				stackTraceMap.put(fileContent, crashReport);
+			else
+				return;
+			Scanner scanner = new Scanner(fileContent);
+			String line = null;
+			CrashLine crashLine = new CrashLine();
+			while (scanner.hasNext()) {
+				line = scanner.nextLine();
+				if (line.startsWith("#")) {
+					stackTraceMap.get(fileContent).addCrashLines(crashLine);
+					crashLine = new CrashLine();
+					Integer index = line.indexOf(" ");
+					Integer crashLineNumber = Integer.parseInt(line.substring(1, index));
+					crashLine.setLineNumber(crashLineNumber);
+				}
+				getAllKeywordsFromLine(line, crashLine);
+			}
+			stackTraceMap.get(fileContent).getCrashLines().add(crashLine);
+			scanner.close();
+		}
+
 	}
 
 	private static void getAllKeywordsFromLine(String line, CrashLine crashLine) {
